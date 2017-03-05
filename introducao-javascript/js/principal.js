91b0e49436a48ab66f413b575172d04f36a3b167
CyberBane <batistajefferson@hotmail.com>
2017-03-05 17:44:55 -0300

    first commit

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,20 @@
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+
+tdImc.textContent = imc;
+
+
+console.log(peso);
+console.log(altura);
+console.log(imc);
