efc12f9a91639fb21b7d7d1ad1c66ed7538e2c0d
CyberBane <batistajefferson@hotmail.com>
2017-03-05 22:15:12 -0300

    commit

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,20 +1,36 @@
 
+var pacientes = document.querySelectorAll(".paciente");
 
-var paciente = document.querySelector("#primeiro-paciente");
+for(var i = 0 ; i < pacientes.length ; i++) {
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+    var paciente = pacientes[i];
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var imc = peso / (altura * altura);
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-tdImc.textContent = imc;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
+    if(peso <= 0 || peso >= 1000) {
+      pesoEhValido = false;
+      tdImc.textContent = "Peso Invalifo";
+      paciente.classList.add("paciente-invalido");
+    }
 
-console.log(peso);
-console.log(altura);
-console.log(imc);
+    if(altura <= 0 || altura >= 3.00){
+      alturaEhValida = false;
+      tdAltura.textContent = "Altura Invalida";
+      paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida) {
+      var imc = peso / (altura * altura);
+      tdImc.textContent = imc.toFixed(2);
+    }
+
+}
